2a2607a3ba949d4ca3c5ffbe835f4a9f7b523328
madhumathiqa <madhumathi.m@gmail.com>
2017-10-16 15:56:50 -0700

    v3

--- hello.java
+++ hello.java
@@ -1,13 +1,14 @@
 
-
 public class hello {
 	public static void main(String args[])
 	{
 		for (int i=0;i<=10;i++)
 		{
 			System.out.println("hai");
-		}
-         System.out.println(“good bye”);
+		
+                 }
+              System.out.println("good-bye");
+         
 	}
 
 }
